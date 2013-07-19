d067ad995cbc5b1018b830adf56183fced1d8e92
Carlo DiCelico <carlodicelico@gmail.com>
2013-07-19 17:27:58 +0000

    update web.js with new port number

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var fs = require('fs');
-
+var myFile = 'index.html';
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   // send HTTP 200 OK
   response.writeHead(200);
   // fs.readFileSync returns a buffer
-  buf = fs.readFileSync('index.html');
+  buf = fs.readFileSync(myFile);
+  // response.end() will stringify the buffer, saving us a call to toString()
   response.end(buf);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
